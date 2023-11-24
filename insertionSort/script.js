c485f477b5d944a536a72a7542ca774d0b072881
Pajjaphon Whanchid <97209810+ZFlucKZ@users.noreply.github.com>
2023-11-24 18:31:41 +0700

    Add : insertion sort

---
+++ insertionSort/script.js
@@ -0,0 +1,22 @@
+function insertionSort(arr) {
+  let currentVal;
+  for (let i = 1; i < arr.length; i++) {
+    currentVal = arr[i];
+    latestIndex = -1;
+
+    for (let j = i - 1; j >= 0; j--) {
+      if (arr[j] > currentVal) {
+        arr[j + 1] = arr[j];
+
+        latestIndex = j;
+      }
+    }
+
+    if (latestIndex !== -1) {
+      arr[latestIndex] = currentVal;
+    }
+  }
+  return arr;
+}
+
+console.log(insertionSort([1, 5, 3, 8, 4, 2, 50, 3, 156, 50, 87, 65]));
